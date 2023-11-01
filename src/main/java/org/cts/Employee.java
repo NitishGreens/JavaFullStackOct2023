ef52ced7b831b6889efc123f1de57b16fc2fc10e
NitishGreens <nitishgreens@gmail.com>
2023-11-01 12:45:41 +0530

    task completed

---
+++ src/main/java/org/cts/Employee.java
@@ -0,0 +1,15 @@
+package org.cts;
+
+public class Employee {
+
+	public void add(int a, int b) {
+		int c = a + b;
+		System.out.println(c);
+	}
+
+	public void sub(int a, int b) {
+		int c = a - b;
+		System.out.println(c);
+	}
+
+}
